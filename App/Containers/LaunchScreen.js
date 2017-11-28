78256cef174d270445f831a84b102a87edcd947f
GavinThomas1192 <gthomas1192@gmail.com>
2017-11-28 15:06:42 -0800

    added react native firebase

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import { ScrollView, Text, Image, View } from 'react-native'
 import { Images } from '../Themes'
+import FullButton from '../Components/FullButton'
+import RootContainer from './RootContainer'
+import App from './Styles/App'
 
 // Styles
 import styles from './Styles/LaunchScreenStyles'
 
 export default class LaunchScreen extends Component {
   render() {
+    const { navigate } = this.props.navigation
     return (
       <View style={styles.mainContainer}>
         <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
@@ -18,8 +22,9 @@ export default class LaunchScreen extends Component {
           <View style={styles.section} >
             <Image source={Images.ready} />
             <Text style={styles.sectionText}>
-              Hi Shaun hi Isaac!.
+              Hi Shaun Hi Isaac!
             </Text>
+            <FullButton text={'Hello World'} onPress={() => navigate('LoginScreen')} />
           </View>
 
         </ScrollView>
--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,10 +1,12 @@
 import { StackNavigator } from 'react-navigation'
+import LoginScreen from '../Containers/LoginScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
-
+import RoundedButton from '../Components/RoundedButton'
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
+  LoginScreen: { screen: LoginScreen, component: RoundedButton },
   LaunchScreen: { screen: LaunchScreen }
 }, {
     // Default config for all screens
