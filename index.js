b4d05a6e92a1ff8e0395dd8ce4f56919b1cbea16
GavinThomas1192 <gthomas1192@gmail.com>
2017-12-02 12:10:52 -0800

    added detailed notes

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ import './App/Config/ReactotronConfig'
 import { AppRegistry } from 'react-native'
 import App from './App/Containers/App'
 
+
+// ********* This is our entry point for both ios and android. *********
+// ********* Now go to ./App/Containers/App *********
 AppRegistry.registerComponent('motoMechanicMeeKanic', () => App)
