a24ecb38e01d6673e68af4332ead18d700b6e0d9
GavinThomas1192 <gthomas1192@gmail.com>
2017-12-21 10:03:44 -0800

    notes in vehicle actions

--- App/Actions/vehicle-actions.js
+++ App/Actions/vehicle-actions.js
@@ -8,98 +8,77 @@ import { userSet, userCreate, userUpdate, loginRequest } from './auth-actions';
 
 
 
-// export const userCreate = user => ({
-//     type: 'USER_CREATE',
-//     payload: user,
-// });
-
-// export const userUpdate = user => ({
-//     type: 'USER_UPDATE',
-//     payload: user,
-// });
-
-
 export const userVehicleCreateRequest = (vehicle, user) => dispatch => {
     console.log('This user got passed through', user, vehicle)
+    //create array to hold vehicles
     let allVehiclesArray = []
+    //Push new vehicle into said array
     allVehiclesArray.push(vehicle[0]);
 
+    //Grab Snapshot at allVehicles to either ADD to or CREATE it on first vehicle. 
     firebase.database().ref('users/' + user.uid + '/allVehicles').once('value').then(function (snapshot) {
-        // ******** This method is straight from their docs ********
-        // ******** It returns whatever is found at the path xxxxx/users/user.uid ********
+        // this will either be null or populated with vehicles. 
         let username = snapshot.val();
         {
+            //If null, set array we created earlier with their first car!
             username === null ? firebase.database().ref('users/' + user.uid + '/allVehicles').set({
                 allVehiclesArray
             }).then(() => {
+                //Then grab new snapshot with this allvehicle array to update our redux store
                 firebase.database().ref('users/' + user.uid).once('value').then(function (snapshot) {
 
                     let username = snapshot.val();
                     console.log(' FOUND THIS USER FROM THE DB', username);
 
+                    //Edge case if database get screwed up, otherwise update redux store
                     { !username.account ? console.log('errrrrrrrrr') : dispatch(userSet(username)) }
                 })
             }) :
 
+                //Otherwise username !== null and we need to add old vehicles with new vehicles. 
+                //Promise because #javascript
                 Promise.all(username.allVehiclesArray.map(ele => {
                     allVehiclesArray.push(ele);
                 })).then(() => {
 
+                    //Store newly constructed array with all their vehicles
                     firebase.database().ref('users/' + user.uid + '/allVehicles').set({
                         allVehiclesArray
                     }).then(() => {
+                        //Grab snapshot to update redux store with all their vehicles. 
                         firebase.database().ref('users/' + user.uid).once('value').then(function (snapshot) {
 
                             let username = snapshot.val();
                             console.log(' FOUND THIS USER FROM THE DB', username);
 
+
+                            //Edge case if database get screwed up, otherwise update redux store
                             { !username.account ? console.log('errrrrrrrrr') : dispatch(userSet(username)) }
                         })
                     })
                 })
 
         }
-        console.log('TESTING BAD ROUTE!', username);
     })
 
 
 
-
-    //     {
-    //         username.length < 2 ? firebase.database().ref('users/' + user.uid + '/allVehicles').set({
-    //             allVehiclesArray
-    //         }).then(() => {
-    //             console.log('user didnt have vehicles, so we created allVehicles')
-    //             loginRequest(user)
-    //         })
-    //             :
-
-    //             allVehiclesArray.push(username.allVehicles)
-
-    //         firebase.database().ref('users/' + user.uid + '/allVehicles').set({
-    //             allVehiclesArray
-    //         }).then(() => {
-    //             loginRequest(user)
-    //             console.log('user already had vehicles, so we added to allVehicles')
-    //         })
-    //     }
-
-    // })
-
 };
 
-export const userVehicleUpdateRequest = (user, vehicle) => dispatch => {
-    let { user } = getState();
-    console.log('_bike_UPDATE_INCOMING_bike', bike)
-    dispatch(bikeUpdate(bike));
+
+//TODO
+// export const userVehicleUpdateRequest = (user, vehicle) => dispatch => {
+//     let { user } = getState();
+//     console.log('_bike_UPDATE_INCOMING_bike', bike)
+//     dispatch(bikeUpdate(bike));
 
 
-};
+// };
 
-export const userVehicleDeleteRequest = (user, vehicle) => dispatch => {
+// export const userVehicleDeleteRequest = (user, vehicle) => dispatch => {
 
-    let { user } = getState();
-    console.log('_bike_DELETE_INCOMING_bike', bike)
-    dispatch(bikeDelete(bike));
+//     let { user } = getState();
+//     console.log('_bike_DELETE_INCOMING_bike', bike)
+//     dispatch(bikeDelete(bike));
 
-};
+// };
